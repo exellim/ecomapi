2e9d0a13de85cda81a41b25067c782d2a2d0c975
exellim <43218303+exellim@users.noreply.github.com>
2021-07-27 21:58:46 +0700

    Model with -a

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiresource('/product', ProductController::class);
+
+Route::group(['prefix'=>'products'],function(){
+    Route::apiresource('/{product}/reviews',ReviewController::class);
 });
